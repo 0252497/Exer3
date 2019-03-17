92f27d9394679da4597286b4014edf990b938de7
Veronique Giguere <0252497@cstjean.qc.ca>
2019-03-17 16:29:18 -0400

    a demain

---
+++ Exer3/Controllers/VoirDocumentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exer3.Controllers
+{
+    public class VoirDocumentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return File("~/documents/Horaires_des_locaux.pdf", "application/pdf");
+        }
+    }
+}
